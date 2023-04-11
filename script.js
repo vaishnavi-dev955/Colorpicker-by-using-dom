52332c6c808376260db010adb458976275f1588d
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-04-11 20:37:38 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function onButton1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0"
+}
+
+
+function onButton2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97"
+}
+
+function onButton3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2"
+}
+
+function onButton4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9"
+}
